e77b5519826f46a58bdfa770ed40ec2f18d4e3e8
Lou <lumeavamo@gmail.com>
2024-01-29 19:39:12 -0500

    My-first

---
+++ client.js
@@ -0,0 +1,38 @@
+const socket = io('http://localhost:80/')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const Name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', Name)
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+    messageContainer.scrollTop = messageContainer.scrollHeight
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+    messageContainer.scrollTop = messageContainer.scrollHeight
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+    messageContainer.scrollTop = messageContainer.scrollHeight
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageContainer.scrollTop = messageContainer.scrollHeight
+    messageInput.value = ''     
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
